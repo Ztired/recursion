9d0a094c87f687d37984184a6538ddc9c717a808
Maxim Zhuk <zhukme84@gmail.com>
2024-02-10 15:52:40 +0500

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+int[] RandomDigitInArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max +1);
+
+    }
+    return array;
+}
+
+void PrintArray(int[] arr, int size)
+{
+    if (size == 0)
+    {
+        Console.Write(arr[size]);
+        return;
+    }
+    Console.Write(arr[size] + " ");
+    PrintArray (arr, size - 1);
+}
+
+Console.Write("Введите размер массива  ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = RandomDigitInArray(n, 0, 100);
+Console.Write($"Массив: {string.Join(" ", array)}");
+Console.WriteLine();
+PrintArray(array, array.Length - 1);
+Console.WriteLine();
+
