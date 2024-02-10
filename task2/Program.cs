20639caa66da3d8c8e15540aa2e06556b81af007
Maxim Zhuk <zhukme84@gmail.com>
2024-02-10 15:55:40 +0500

    correction task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -9,14 +9,14 @@ int n = Convert.ToInt32(Console.ReadLine());
 
 int GetAkkerman(int m, int n)
 {
-if (m == 0) return n + 1;
-else if (n == 0)
-{
-  return GetAkkerman(m - 1, 1);  
-} 
-else 
-{
+  if (m == 0) return n + 1;
+  else if (n == 0)
+  {
+    return GetAkkerman(m - 1, 1);  
+  } 
+  else 
+  {
     return GetAkkerman(m - 1, GetAkkerman(m, n - 1));
-}
+  }
 }
 Console.Write($"Функция Аккермана равна {GetAkkerman(m, n)} ");
