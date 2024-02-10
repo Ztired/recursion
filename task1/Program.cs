1d7d647906546a352b9c08623037563502f386cf
Maxim Zhuk <zhukme84@gmail.com>
2024-02-10 14:13:36 +0500

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+int M = 15;
+int N = 85;
+
+void ShowNaturalNumbers(int min, int max)
+{
+    if (min == max+1) return;
+    Console.Write(min + " ");
+    ShowNaturalNumbers(min + 1,N);
+}
+ShowNaturalNumbers(M,N);
